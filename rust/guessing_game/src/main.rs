44bb797b00ab300c30c39a96a1ac52fd014986e2
FilippKramer <krame490@gmail.com>
2019-03-26 20:35:33 -0700

    Finish variables, shadowing, mutation, collection, iters, statements, expressions

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -44,17 +44,39 @@
 // }
 //  signed numbers stored with twos complement
 
-fn main() {
+// fn another_function(x: i32) {
+//   println!("this is another {} function", x);
+// }
+
+// fn five() -> i32 {
+//   5
+// }
+
+// fn main() {
   // let mut x: u8 = 280;
-  let guess: u32 = "42".parse().expect("Not a number!");
-  println!("The variable x is {}", guess);
+  // let guess: u32 = "42".parse().expect("Not a number!");
+  // println!("The variable x is {}", guess);
 
-  let tup = (500, 6.4, 1);
+  // let tup = (500, 6.4, 1);
 
-  let (x, y, z) = tup;
+  // let (x, y, z) = tup;
 
-  println!("The value of y is {}", y);
-}
+  // println!("The value of x, y, z is {}, {}, {}", x, y, z);
+
+  // another_function(5);
+  // let x = (5 + 5);
+//   let x = five();
+
+//   println!("The value of x is: {}", x);
+// }
 
 //arrays in rust have fixed length and all elements must be same type
 //tuples fixed length and can have different element types
+
+fn main() {
+  for number in (1..4).rev() {
+    println!("{}!", number);
+  }
+
+  println!("LIFTOFF!");
+}
